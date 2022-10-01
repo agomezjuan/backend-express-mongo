e93a130b9d304d440517fc5dbc5cc66810b346fc
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-01 17:40:39 +0200

    cors para permitir conexiones desde el front

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,6 +19,20 @@ mongoose
 // creacion del servidor
 const app = express();
 
+// cors para permitir peticiones desde el front
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // body parser
 app.use(express.json());
 
