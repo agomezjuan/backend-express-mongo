aa73dff41ee612e97dc8f05a19ead051e970a21d
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-10-01 17:40:17 +0200

    error handler

--- src/index.js
+++ src/index.js
@@ -22,17 +22,14 @@ const errorHandler = error => {
   const address = server.address();
 
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
-
       process.exit(1);
-      break;
     case "EADDRINUSE":
       console.error(bind + " is already in use");
-
       process.exit(1);
-      break;
     default:
       throw error;
   }
